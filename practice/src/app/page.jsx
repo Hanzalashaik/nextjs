c10c1836c3f49e56c0a8c95ca645bb9672e1eaca
Hanzalashaik <shaikhhanzala27@gmail.com>
2024-05-15 17:58:50 +0530

    dynamic route

--- practice/src/app/page.jsx
+++ practice/src/app/page.jsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hanzala nextjs</h1>
+      <h1>Hello Hanzala</h1>
     </main>
   );
 }
